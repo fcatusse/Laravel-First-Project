bab2dd56d0672895ce2bcfa12aa7888959a32820
unknown <francois.catusse@gmail.com>
2018-12-07 10:12:56 +0100

    language

--- sports/resources/views/admin/team_addForm.blade.php
+++ sports/resources/views/admin/team_addForm.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     
-    <h2>Admin Teams add</h2>
+    <h2>{{ __('Add a team') }}</h2>
 
     @if ($errors->any())
     <div class="alert alert-danger">
@@ -17,11 +17,11 @@
     <form method="POST" action="">
         @csrf
         <div class="form-group">
-            <label>Name</label><br>
+            <label>{{ __('Name') }}</label><br>
             <input class="form-control" type="text" name="name" placeholder="Enter team name">
         </div>
         <div class="form-group">
-            <label>Country</label><br>
+            <label>{{ __('Country') }}</label><br>
             <select class="form-control" name="country">
                 @foreach ($data_countries as $key => $value)
                     <option value="{{ $key }}">{{ $value }}</option>
@@ -29,15 +29,15 @@
             </select>
         </div>
         <div class="form-group">
-            <label>Matches won</label><br>
+            <label>{{ __('Matches won') }}</label><br>
             <input class="form-control" type="number" name="matchs_won" placeholder="Enter mumber matches lon">
         </div>
         <div class="form-group">
-            <label>Matches lose</label><br>
+            <label>{{ __('Matches lose') }}</label><br>
             <input class="form-control" type="number" name="matchs_lose" placeholder="Enter mumber matches lose">
         </div>
         <div class="form-group">
-            <label>Matches null</label><br>
+            <label>{{ __('Matches null') }}</label><br>
             <input class="form-control" type="number" name="matchs_null" placeholder="Enter mumber matches null">
         </div>
          <button type="submit" class="btn btn-primary">Submit</button>
